b41bcf7c09a320b39fe8682e03b04026ff72ff82
ATRIKA SHOW <readeratrika08@gmail.com>
2024-06-29 00:56:26 +0530

    HomePage Completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ import {
   RouterProvider
 } from "react-router-dom"
 import Homepage from "./Frontend/Components/Homepage";
+import SearchPage from "./Frontend/Components/SearchPage";
 
 function App() {
   const appRouter = createBrowserRouter([
     {
       path : "/",
       element : <Homepage/>
+    },
+    {
+      path : "/search",
+      element : <SearchPage/>
     }
   ])
 
---
+++ src/Frontend/Components/SearchPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SearchPage() {
+  return (
+    <div>
+      search page
+    </div>
+  )
+}
