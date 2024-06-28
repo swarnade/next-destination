3770521b2d36aed12b6de67c54b006485df09994
ATRIKA SHOW <readeratrika08@gmail.com>
2024-06-29 00:02:23 +0530

    Added custom color in tailwind

--- src/Frontend/Components/Homepage.jsx
+++ src/Frontend/Components/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Homepage() {
   return (
-    <div>
+    <div className='bg-mainBgColor text-white'>
       Homepage
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBgColor: '#000123', // You can name your color and provide the hex code
+        yellowColor: '#FFDE59',
+        btnColor : '#5CE1E6',
+        // Add more custom colors here
+      },
+    },
   },
   plugins: [],
 };
