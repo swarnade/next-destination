a9bc69bf978ba31d8e094a964389df9ecc1d4b4f
ATRIKA SHOW <readeratrika08@gmail.com>
2024-06-29 02:23:42 +0530

    Search page complete

--- src/Frontend/Components/SearchPage.jsx
+++ src/Frontend/Components/SearchPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
 export default function SearchPage() {
   return (
-    <div>
-      search page
+    <div className='bg-mainBgColor w-full h-screen'>
+      <input
+      className='w-2/4 p-2 pl-5 bg-gray-500 text-gray-100 font-bold text-lg ml-80 mt-40 rounded-md'
+       type="text" name="" id="" placeholder='Explore Your Next Destination'
+       
+       />
     </div>
   )
 }
