5749912095338ad8ae36f833d609ea5c4ba10f6d
ATRIKA SHOW <readeratrika08@gmail.com>
2024-06-28 23:23:05 +0530

    added deps

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Tip planner</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
