4dc763ba3474e7c8a09446a889038df7146d4d0d
anupam <anupammahato357@gmail.com>
2024-06-29 15:40:12 +0530

    Weather information  Styling

--- src/Frontend/Components/WeatherCard.jsx
+++ src/Frontend/Components/WeatherCard.jsx
@@ -5,32 +5,21 @@ const WeatherCard = ({ data }) => {
   console.log(data?.coord);
 
   return (
-    <div className="weather-card p-4 m-2 mw-w-sm mx-auto bg-white rounded-xl shadow-md overflow-hidden">
-      <h2>Weather Information for {data?.name}</h2>
-      
-      <div className="section">
-        <strong>Weather:</strong>
-        
-        Description: {data?.weather[0].description}
-        <br />
-        <br />
-        Temperature: {data?.main.temp} K<br />
-        Feels Like: {data?.main.feels_like} K<br />
-        <img
-          src={`http://openweathermap.org/img/wn/${data?.weather[0].icon}.png`}
-          alt="Weather Icon"
-        />
-         
-      </div>
-        
-      <div className="section">
-        <strong>Clouds:</strong>
-        <br />
-        Cloudiness: {data?.clouds.all}%
-      </div>
-     
+    <div className="weather-card p-4 mw-w-sm mx-auto bg-mainBgColor min-h-screen text-white rounded-xl shadow-md overflow-hidden">
+      <h2 className=" text-center">Weather Information of {data?.name}</h2>
 
-     
+      <div className="flex justify-center gap-6 mt-4 text-sm">
+        <div> • {data?.weather[0].description} </div>
+
+        <div> Temperature: {data?.main.temp} K </div>
+        <div>
+          {" "}
+          Feels Like: {data?.main.feels_like} K<br />{" "}
+        </div>
+        {/* 
+         */}
+        <div className="">Cloudiness: {data?.clouds.all}%</div>
+      </div>
     </div>
   );
 };
