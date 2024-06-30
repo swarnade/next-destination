4e93efc9cb10133e8a2b1533f503bd202502bf84
ATRIKA SHOW <readeratrika08@gmail.com>
2024-06-30 10:48:19 +0530

    dynamic itinerary

--- src/Frontend/Components/Iternary.jsx
+++ src/Frontend/Components/Iternary.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export default function Iternary({
   checkIn,
@@ -9,6 +10,7 @@ export default function Iternary({
   setLoading,
 }) {
   const [iternaryData, setIternaryData] = useState(null);
+  const { place } = useParams();
 
   useEffect(() => {
     if (checkIn && checkOut && budget) {
@@ -20,7 +22,7 @@ export default function Iternary({
         .get(
           `${
             import.meta.env.VITE_SERVER
-          }/itinerary?place=Delhi&cin=${checkIn}&cout=${checkOut}&budget=${budget}`
+          }/itinerary?place=${place}&cin=${checkIn}&cout=${checkOut}&budget=${budget}`
         )
         .then((res) => setIternaryData(res.data.itinerary))
         .catch((err) => console.log(err));
